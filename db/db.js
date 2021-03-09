4e9814567486dd764bf3cd36705d6287908fdcf0
rnick1 <rnick0260@gmail.com>
2021-03-08 20:12:43 -0800

    Atttempt to get the app.delete to work.

--- db/db.js
+++
@@ -1,9 +0,0 @@
-const toDosArray = [
-    {
-        id: "1",
-        title: "Test Title",
-        text: "Test text"
-    }
-]
-
-module.exports = toDosArray;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,43 @@
 // Loads the data.
-const db = require('../db/db');
+const db = require('../db/db.json');
 const fs = require('fs');
-const toDosArray = require('../db/db');
 const uniqueID = require('uniqid');
 
 // Routing
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(db));
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('../db/db.json', function (err, data) {
+            if (err) return console.log(err);
+            res.json(data);
+        });
+    });
 
     app.post('/api/notes', (req, res) => {
         const newToDo = req.body;
         newToDoID = uniqueID();
         console.log(newToDo);
-        toDosArray.push(newToDo);
+        db.push(newToDo);
         res.json(newToDo);
     });
 
-    app.delete('/api/notes', (req, res) => {
-        toDosArray.delete
-
-        res.json({ ok: true });
+    app.delete("/api/notes/:id", function (req, res) {
+        notes.splice(req.params.id, 1);
+        change();
+        console.log("Deleted todo with id " + req.params.id);
     });
+
+    function change() {
+        fs.writeFile("db/db.json", JSON.stringify(db, '\t'), err => {
+            if (err) throw err;
+            return true;
+        });
+    }
 };
+
+
+
+
 
 // // Routing
 // module.exports = (app) => {
-//     app.get('/api/notes', (req, res) => {
-//         fs.readFile('../db/db', function (err, data) {
-//             if (err) return console.log(err);
-//             res.json(data);
-//         });
-//     });
+//     app.get('/api/notes', (req, res) => res.json(db));
