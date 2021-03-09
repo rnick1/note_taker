7e38cc641a23d6936237ca07fbcf04323f35ac03
rnick1 <rnick0260@gmail.com>
2021-03-09 09:59:20 -0800

    Added post and delete requests that work.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,27 @@
 // Loads the data.
 const db = require('../db/db.json');
-const fs = require('fs');
 const uniqueID = require('uniqid');
 
 // Routing
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        fs.readFile('../db/db.json', function (err, data) {
-            if (err) return console.log(err);
-            res.json(data);
-        });
+        res.json(db);
     });
 
     app.post('/api/notes', (req, res) => {
         const newToDo = req.body;
-        newToDoID = uniqueID();
+        newToDo.id = uniqueID();
         console.log(newToDo);
         db.push(newToDo);
         res.json(newToDo);
     });
 
     app.delete("/api/notes/:id", function (req, res) {
-        notes.splice(req.params.id, 1);
-        change();
-        console.log("Deleted todo with id " + req.params.id);
+        db.forEach(function (element, index) {
+            if (element.id === req.params.id) {
+                db.splice(index, 1)
+                res.json(db)
+            }
+        })
     });
-
-    function change() {
-        fs.writeFile("db/db.json", JSON.stringify(db, '\t'), err => {
-            if (err) throw err;
-            return true;
-        });
-    }
-};
-
-
-
-
-
-// // Routing
-// module.exports = (app) => {
-//     app.get('/api/notes', (req, res) => res.json(db));
+}
